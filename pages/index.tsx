efff920c9d573c3838b3042eed6292cec797bdfa
Hugo Souza <hugo_sbo04@hotmail.com>
2022-07-03 21:48:26 -0300

    Add

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,113 +41,6 @@ interface IModalProduto extends IModal {
   callbackModalItem: any;
 }
 
-
-class Home extends React.Component {
-  state = {
-    caixa_status: "CAIXA ABERTO",
-    cliente_venda: {},
-    itens: [] as IItemPedido[],
-    total: 0,
-    showModalAjuda: false,
-    showModalItemCadastro: false,
-    showModalPessoaCadastro: false,
-  };
-
-  componentDidMount = () => {
-    const onKeyDown = (e: any) => {
-      switch (e.keyCode) {
-        // F2
-        case 113:
-          this.setState({ showModalPessoaCadastro: true })
-          break;
-        // F4
-        case 115:
-          alert("Abrir o modal do cliente mas filtrando vendedor...");
-          break;
-        // F10
-        case 121:
-          alert("F10 pressionado... Abrir tela de finalização!");
-          break;
-        // F12
-        case 123:
-          if (e.ctrlKey) {
-            this.setState({ showModalAjuda: true });
-            e.preventDefault();
-          }
-          break;
-      }
-    };
-
-    document.body.addEventListener("keydown", onKeyDown);
-  };
-
-  callBackProdutoDetalheHome = (it: IItemPedido) => {
-    // com o objeto recebido do ProdutoDetalheHome, é concatenado num array de state que passa esse state para o componente de preenchimento do grid.
-    let subtotal = (it.preco ? it.preco : 0) * (it.quantidade ? it.quantidade : 0);
-    console.log(it)
-    it.sequencia = this.state.itens.length + 1;
-    it.subtotal = subtotal;
-
-    this.setState({ itens: [...this.state.itens, it] });
-    this.setState({ total: (this.state.total += subtotal) });
-  };
-
-  render() {
-    return (
-      <>
-        <div className={styles.home_body}>
-          <Cabecalho
-            key={"cabecalho"}
-            onShowModalItemCadastro={() => this.setState({ showModalItemCadastro: true })}
-          />
-          <div className={styles.box_content}>
-            <div className={styles.box_esquerdo}>
-              <DetalheConsumidor
-                caixa_status={this.state.caixa_status}
-              />
-              <TituloBox label={"Detalhe"} type_img={"detail"} />
-              <div className="w-100 h-100 d-flex flex-row bg-light overflow-auto p-2">
-                <div className="row">
-                  <div className="col">
-                    // Incluir a imagem.
-                  </div>
-                  <div className="col">
-                    <DetalheProduto
-                      callBackProdutoDetalheHome={this.callBackProdutoDetalheHome}
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className={styles.box_direito}>
-              <TituloBox label={"Produtos"} type_img={"product"} />
-              <TabelaProdutos itens={this.state.itens} />
-              <Totalizador total={this.state.total} />
-            </div>
-          </div>
-        </div>
-
-        <ModalAjuda
-          onClose={() => this.setState({ showModalAjuda: false })}
-          showModal={this.state.showModalAjuda}
-          title={"Ajuda - Comandos"}
-        />
-
-        <ModalItem
-          onClose={() => this.setState({ showModalItemCadastro: false })}
-          showModal={this.state.showModalItemCadastro}
-        />
-
-        <ModalPessoa
-          onClose={() => this.setState({ showModalPessoaCadastro: false })}
-          showModal={this.state.showModalPessoaCadastro}
-          clientSelected={(res: any) => this.setState({ cliente_venda: res })}
-        />
-      </>
-    );
-  }
-}
-
 const Cabecalho = (props: ICabecalhoHome) => {
   function handleMenu(props: ICabecalhoHome): React.ReactNode {
     const itens = [
@@ -614,6 +507,112 @@ class ModalSelecaoItem extends React.Component<IModalProduto, {}> {
       >
         {this.handleTable()}
       </Modal>
+    );
+  }
+}
+
+class Home extends React.Component {
+  state = {
+    caixa_status: "CAIXA ABERTO",
+    cliente_venda: {},
+    itens: [] as IItemPedido[],
+    total: 0,
+    showModalAjuda: false,
+    showModalItemCadastro: false,
+    showModalPessoaCadastro: false,
+  };
+
+  componentDidMount = () => {
+    const onKeyDown = (e: any) => {
+      switch (e.keyCode) {
+        // F2
+        case 113:
+          this.setState({ showModalPessoaCadastro: true })
+          break;
+        // F4
+        case 115:
+          alert("Abrir o modal do cliente mas filtrando vendedor...");
+          break;
+        // F10
+        case 121:
+          alert("F10 pressionado... Abrir tela de finalização!");
+          break;
+        // F12
+        case 123:
+          if (e.ctrlKey) {
+            this.setState({ showModalAjuda: true });
+            e.preventDefault();
+          }
+          break;
+      }
+    };
+
+    document.body.addEventListener("keydown", onKeyDown);
+  };
+
+  callBackProdutoDetalheHome = (it: IItemPedido) => {
+    // com o objeto recebido do ProdutoDetalheHome, é concatenado num array de state que passa esse state para o componente de preenchimento do grid.
+    let subtotal = (it.preco ? it.preco : 0) * (it.quantidade ? it.quantidade : 0);
+    console.log(it)
+    it.sequencia = this.state.itens.length + 1;
+    it.subtotal = subtotal;
+
+    this.setState({ itens: [...this.state.itens, it] });
+    this.setState({ total: (this.state.total += subtotal) });
+  };
+
+  render() {
+    return (
+      <>
+        <div className={styles.home_body}>
+          <Cabecalho
+            key={"cabecalho"}
+            onShowModalItemCadastro={() => this.setState({ showModalItemCadastro: true })}
+          />
+          <div className={styles.box_content}>
+            <div className={styles.box_esquerdo}>
+              <DetalheConsumidor
+                caixa_status={this.state.caixa_status}
+              />
+              <TituloBox label={"Detalhe"} type_img={"detail"} />
+              <div className="w-100 h-100 d-flex flex-row bg-light overflow-auto p-2">
+                <div className="row">
+                  <div className="col">
+                    // Incluir a imagem.
+                  </div>
+                  <div className="col">
+                    <DetalheProduto
+                      callBackProdutoDetalheHome={this.callBackProdutoDetalheHome}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.box_direito}>
+              <TituloBox label={"Produtos"} type_img={"product"} />
+              <TabelaProdutos itens={this.state.itens} />
+              <Totalizador total={this.state.total} />
+            </div>
+          </div>
+        </div>
+
+        <ModalAjuda
+          onClose={() => this.setState({ showModalAjuda: false })}
+          showModal={this.state.showModalAjuda}
+          title={"Ajuda - Comandos"}
+        />
+
+        <ModalItem
+          onClose={() => this.setState({ showModalItemCadastro: false })}
+          showModal={this.state.showModalItemCadastro}
+        />
+
+        <ModalPessoa
+          onClose={() => this.setState({ showModalPessoaCadastro: false })}
+          showModal={this.state.showModalPessoaCadastro}
+          clientSelected={(res: any) => this.setState({ cliente_venda: res })}
+        />
+      </>
     );
   }
 }
